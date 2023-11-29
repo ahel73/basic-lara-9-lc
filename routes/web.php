1caba7c79d8ebe22e73f1192d8e97c500d1e60e6
Нагаев Алексей <nag495@yandex.ru>
2023-11-29 20:50:59 +0300

    37 установили Laravel Breeze для авторизации и регистрации подправил установочные пакеты так как некоторых не было

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -8,8 +9,8 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
 |
 */
 
@@ -17,10 +18,14 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/workers', [\App\Http\Controllers\WorkerController::class, 'index'])->name('workers.index');
-Route::get('/workers/create', [\App\Http\Controllers\WorkerController::class, 'create'])->name('workers.create');
-Route::get('/workers/{worker}', [\App\Http\Controllers\WorkerController::class, 'show'])->name('workers.show');
-Route::post('/workers', [\App\Http\Controllers\WorkerController::class, 'store'])->name('workers.store');
-Route::get('/workers/{worker}/edit', [\App\Http\Controllers\WorkerController::class, 'edit'])->name('workers.edit');
-Route::patch('/workers/{worker}', [\App\Http\Controllers\WorkerController::class, 'update'])->name('workers.update');
-Route::delete('/workers/{worker}', [\App\Http\Controllers\WorkerController::class, 'delete'])->name('workers.delete');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
